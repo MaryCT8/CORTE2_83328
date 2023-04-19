a88010239ebfe1ec177442092ae5c563889307ed
Mary <amctsym@gmail.com>
2023-04-18 22:10:29 -0500

    Homework formato uml

---
+++ TAREA_SESION14/formato_UML.py
@@ -0,0 +1,13 @@
+#Ciudadano
+#     - nombre:str
+#     - edad:int
+#     - cedula:int
+#     + set_nombre(nombre:str):None
+#     + set_cedula(cedula:int):None
+#     + set_edad(edad:int):None
+#     + get_nombre():str
+#     + get_cedula():int
+#     + get_edad():int
+#     + mostrar():str
+#     + es_mayor_de_edad():bool
+     
